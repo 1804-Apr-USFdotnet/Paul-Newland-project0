63246e888c5dca10f363e01e3d9ed6da69ad54c3
newlandp <paulnewland@hotmail.com>
2018-04-30 14:42:42 -0400

    Add tests to relevent Helper Lib functions (business logic that was on requirements)

--- ConsoleRestaurantsProject0/Lib/HelperLibrary.cs
+++ ConsoleRestaurantsProject0/Lib/HelperLibrary.cs
@@ -9,15 +9,15 @@ namespace ConsoleRestaurantsProject0.Lib
 {
     public class HelperLibrary
     {
-        public static IEnumerable<Restaurant> SortRestaurantsByName(IEnumerable<Restaurant> restaurants)
+        public static List<Restaurant> SortRestaurantsByName(IEnumerable<Restaurant> restaurants)
         {
-            return restaurants.OrderBy(r => r.Name);
+            return restaurants.OrderBy(r => r.Name).ToList();
         }
 
-        public static IEnumerable<Restaurant> SearchRestaurantsByName(IEnumerable<Restaurant> restaurants,
+        public static List<Restaurant> SearchRestaurantsByName(IEnumerable<Restaurant> restaurants,
             string searchValue)
         {
-            return restaurants.Where(r => r.Name.Contains(searchValue));
+            return restaurants.Where(r => r.Name.ToLower().Contains(searchValue.ToLower())).ToList();
         }
     }
 }
--- Project0Test/Project0HelperLibraryTests.cs
+++ Project0Test/Project0HelperLibraryTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using ConsoleRestaurantsProject0.DataModels;
+using ConsoleRestaurantsProject0.Lib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Project0Test
@@ -17,10 +18,90 @@ namespace Project0Test
             new Restaurant(){Name = "C Restaurant 5 Special", Id = 5}
         };
 
-        [TestMethod]
-        public void SortRestaurantsByNameReturnsCorrectlyOrderedRestaurants()
+        private List<Restaurant> _restaurantsSorted = new List<Restaurant>()
         {
-            
+            new Restaurant(){Name = "A Restaurant 3", Id = 3},
+            new Restaurant(){Name = "B Restaurant 1", Id = 1},
+            new Restaurant(){Name = "C Restaurant 5 Special", Id = 5},
+            new Restaurant(){Name = "D Restaurant 4", Id = 4},
+            new Restaurant(){Name = "E Restaurant 2", Id = 2}
+        };
+
+        [TestMethod]
+        public void SortRestaurantsByNameReturnsCorrectlyOrderedRestaurants0()
+        {
+            var list = HelperLibrary.SortRestaurantsByName(_restaurants);
+            Assert.AreEqual(list[0].Id, _restaurantsSorted[0].Id);
+        }
+
+        [TestMethod]
+        public void SortRestaurantsByNameReturnsCorrectlyOrderedRestaurants1()
+        {
+            var list = HelperLibrary.SortRestaurantsByName(_restaurants);
+            Assert.AreEqual(list[1].Id, _restaurantsSorted[1].Id);
+        }
+
+        [TestMethod]
+        public void SortRestaurantsByNameReturnsCorrectlyOrderedRestaurants2()
+        {
+            var list = HelperLibrary.SortRestaurantsByName(_restaurants);
+            Assert.AreEqual(list[2].Id, _restaurantsSorted[2].Id);
+        }
+
+        [TestMethod]
+        public void SortRestaurantsByNameReturnsCorrectlyOrderedRestaurants3()
+        {
+            var list = HelperLibrary.SortRestaurantsByName(_restaurants);
+            Assert.AreEqual(list[3].Id, _restaurantsSorted[3].Id);
+        }
+
+        [TestMethod]
+        public void SortRestaurantsByNameReturnsCorrectlyOrderedRestaurants4()
+        {
+            var list = HelperLibrary.SortRestaurantsByName(_restaurants);
+            Assert.AreEqual(list[4].Id, _restaurantsSorted[4].Id);
+        }
+
+        [TestMethod]
+        public void SortRestaurantsByNameDoesNotReturnUnCorrectlyOrderedRestaurants0()
+        {
+            var list = HelperLibrary.SortRestaurantsByName(_restaurants);
+            Assert.AreNotEqual(list[3].Id, _restaurantsSorted[4].Id);
+        }
+
+        [TestMethod]
+        public void SortRestaurantsByNameDoesNotReturnUnCorrectlyOrderedRestaurants1()
+        {
+            var list = HelperLibrary.SortRestaurantsByName(_restaurants);
+            Assert.AreNotEqual(list[4].Id, _restaurantsSorted[2].Id);
+        }
+
+        [TestMethod]
+        public void SearchRestaurantsByNameReturnsCorrectRestaurants0()
+        {
+            var list = HelperLibrary.SearchRestaurantsByName(_restaurants, "3");
+            Assert.AreEqual(list[0].Id, 3);
+        }
+
+        [TestMethod]
+        public void SearchRestaurantsByNameReturnsCorrectRestaurants1()
+        {
+            var list = HelperLibrary.SearchRestaurantsByName(_restaurants, "Restaurant");
+            Assert.AreEqual(list.Count, 5);
+        }
+
+        [TestMethod]
+        public void SearchRestaurantsByNameDoesNotReturnUnCorrectRestaurants0()
+        {
+            var list = HelperLibrary.SearchRestaurantsByName(_restaurants, "3");
+            Assert.AreNotEqual(list[0].Id, 2);
+        }
+
+        [TestMethod]
+        public void SearchRestaurantsByNameDoesNotReturnUnCorrectRestaurants1()
+        {
+            var list = HelperLibrary.SearchRestaurantsByName(_restaurants, "Restaurant");
+            Assert.AreNotEqual(list.Count, 4);
         }
     }
 }
